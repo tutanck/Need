6a818775b83e348f02dd17c8774ab0b6559f372d
tutanck <tutanck@gmail.com>
2017-11-15 11:12:54 +0100

    _

--- app/src/main/java/com/aj/need/db/colls/USERS.java
+++ app/src/main/java/com/aj/need/db/colls/USERS.java
@@ -1,26 +1,10 @@
 package com.aj.need.db.colls;
 
-import android.util.Log;
-
 import com.aj.need.db.IO;
 import com.aj.need.db.colls.itf.Coll;
 
-import com.aj.need.domain.components.profile.UserProfile;
-import com.aj.need.tools.utils.__;
-
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Transaction;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.aj.need.db.IO.db;
 
 /**
  * Created by joan on 02/10/2017.
@@ -56,49 +40,8 @@ public final class USERS implements Coll {
         return getUserRef(IO.getCurrentUserUid());
     }
 
-
     public final static CollectionReference getColl() {
         return IO.db.collection(coll);
     }
-
-    /*public static Task<List<UserProfile>> computeUsersInfo(final QuerySnapshot result) {
-
-        return db.runTransaction(new Transaction.Function<List<UserProfile>>() {
-            @Override
-            public List<UserProfile> apply(final Transaction transaction) throws FirebaseFirestoreException {
-
-                final List<UserProfile> contacts = new ArrayList<>();
-
-                for (DocumentSnapshot contactDoc : result)
-                    try {
-                        Log.d("computeUsersInfo", contactDoc.getId() + " => " + contactDoc.getData());
-
-                        DocumentReference userRef = USERS.getUserRef(contactDoc.getId());
-                        DocumentSnapshot userDoc = transaction.get(userRef);
-
-                        //!important : each doc read in a transaction must be also wrote
-                        transaction.update(userRef, "lastRead", FieldValue.serverTimestamp());
-
-                        contacts.add(new UserProfile(
-                                        userDoc.getId()
-                                        , userDoc.getString(USERS.usernameKey)
-                                        , userDoc.getLong(USERS.avgRatingKey).intValue()
-                                        , userDoc.getLong(USERS.availabilityKey).intValue()
-                                        , contactDoc.getString(MESSAGES.conversationIDKey)
-                                        , contactDoc.getString(MESSAGES.messageKey)
-                                        , contactDoc.getDate(MESSAGES.dateKey)
-                                )
-                        );
-
-                    } catch (FirebaseFirestoreException e) {
-                        e.printStackTrace();
-                        __.fatal("TR Failed : computeUserContacts : FirebaseFirestoreException");//// TODO: 15/10/2017 !important !urgent smooth stop then present retry to user
-                        //// TODO: 15/10/2017
-                    }
-
-                return contacts;
-            }
-        });
-    }*/
 
 }
